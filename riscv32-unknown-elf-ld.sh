b0b2ffaa9e2cb2f18c23557d97f5a9e0ef6b2429
Nathan Holstein <nathan.holstein@gmail.com>
2020-02-03 00:28:10 -0500

    Use global_asm!() to avoid a distinct assembly file.

--- riscv32-unknown-elf-ld.sh
+++ riscv32-unknown-elf-ld.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-riscv32-unknown-elf-ld -static --no-dynamic-linker -nostdlib --gc-sections -Tflash_link.ld flash_crt.o "$@"
+exec riscv32-unknown-elf-ld \
+	-static \
+	--no-dynamic-linker \
+	-nostdlib \
+	--gc-sections \
+	-Tflash_link.ld \
+	"$@"
